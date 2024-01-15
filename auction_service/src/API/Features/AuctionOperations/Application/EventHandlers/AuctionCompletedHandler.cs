573abed69d217bba15bcbbfa1dd9366132a0ca87
Archit3ct <timothy.s.rasmussen@gmail.com>
2024-01-15 13:18:44 +0100

    Event Publishing Working

--- auction_service/src/API/Features/AuctionOperations/Application/EventHandlers/AuctionCompletedHandler.cs
+++ auction_service/src/API/Features/AuctionOperations/Application/EventHandlers/AuctionCompletedHandler.cs
@@ -1,4 +1,6 @@
+using API.Features.AuctionOperations.Application.EventHandlers.Topics;
 using API.Features.AuctionOperations.Domain.Events;
+using Infrastructure.Persistence._Interfaces;
 using MediatR;
 
 namespace API.Features.AuctionOperations.Application.EventHandlers;
@@ -6,15 +8,26 @@ namespace API.Features.AuctionOperations.Application.EventHandlers;
 public class AuctionCompletedHandler : INotificationHandler<AuctionCompletedEvent>
 {
     private readonly ILogger<AuctionCompletedHandler> _logger;
+    private readonly IIntegrationEventHandler _eventHandler;
 
-    public AuctionCompletedHandler(ILogger<AuctionCompletedHandler> logger)
+    public AuctionCompletedHandler(
+        ILogger<AuctionCompletedHandler> logger,
+        IIntegrationEventHandler eventHandler)
     {
         _logger = logger;
+        _eventHandler = eventHandler;
     }
     
-    public Task Handle(AuctionCompletedEvent notification, CancellationToken cancellationToken)
+    public async Task Handle(AuctionCompletedEvent notification, CancellationToken cancellationToken)
     {
         _logger.LogInformation(notification.Message);
-        return Task.CompletedTask;
+        
+        var integrationEvent = new AuctionCompletedIntEvent
+        {
+            AuctionId = notification.AuctionId,
+            CompletionTime = notification.CompletionTime
+        };
+
+        await _eventHandler.PublishProtobufEventAsync(integrationEvent);
     }
 }
--- auction_service/src/API/Features/AuctionOperations/Application/EventHandlers/AuctionStartedHandler.cs
+++ auction_service/src/API/Features/AuctionOperations/Application/EventHandlers/AuctionStartedHandler.cs
@@ -1,4 +1,6 @@
+using API.Features.AuctionOperations.Application.EventHandlers.Topics;
 using API.Features.AuctionOperations.Domain.Events;
+using Infrastructure.Persistence._Interfaces;
 using MediatR;
 
 namespace API.Features.AuctionOperations.Application.EventHandlers;
@@ -6,15 +8,26 @@ namespace API.Features.AuctionOperations.Application.EventHandlers;
 public class AuctionStartedHandler : INotificationHandler<AuctionStartedEvent>
 {
     private readonly ILogger<AuctionStartedHandler> _logger;
+    private readonly IIntegrationEventHandler _eventHandler;
 
-    public AuctionStartedHandler(ILogger<AuctionStartedHandler> logger)
+    public AuctionStartedHandler(
+        ILogger<AuctionStartedHandler> logger,
+        IIntegrationEventHandler eventHandler)
     {
         _logger = logger;
+        _eventHandler = eventHandler;
     }
 
-    public Task Handle(AuctionStartedEvent notification, CancellationToken cancellationToken)
+    public async Task Handle(AuctionStartedEvent notification, CancellationToken cancellationToken)
     {
         _logger.LogInformation(notification.Message);
-        return Task.CompletedTask;
+        
+        var integrationEvent = new AuctionStartedIntEvent
+        {
+            AuctionId = notification.AuctionId,
+            StartTime = notification.StartTime
+        };
+
+        await _eventHandler.PublishProtobufEventAsync(integrationEvent);
     }
 }
